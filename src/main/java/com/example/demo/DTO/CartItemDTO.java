40ddf4a84d0bd4898c73d7d11cfd200edb3aa920
Ilya Alekseev <Hotway97@yandex.ru>
2023-10-14 11:45:20 +0300

    for render

--- src/main/java/com/example/demo/DTO/CartItemDTO.java
+++ src/main/java/com/example/demo/DTO/CartItemDTO.java
@@ -3,6 +3,7 @@ package com.example.demo.DTO;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 public class CartItemDTO {
--- src/main/java/com/example/demo/DTO/UpdateCartItemDTO.java
+++ src/main/java/com/example/demo/DTO/UpdateCartItemDTO.java
@@ -10,3 +10,5 @@ public class UpdateCartItemDTO {
     public int count;
 }
 
+
+
